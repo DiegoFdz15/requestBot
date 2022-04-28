99e87c49085966579a8728db689ef994a0d3bf37
DiegoFdz <diegofernandezf06@gmail.com>
2022-04-28 12:54:58 -0400

    update

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: python a.py
-
-worker: python a.py 
--- a.py
+++ a.py
@@ -1,31 +1,50 @@
+from datetime import datetime
+from numpy import integer
+import requests
+import time
+import argparse
+
 def main():
+
+    parser = argparse.ArgumentParser(description='''Usalo para mantener activo una aplicacion de heroku,
+    realiza peticiones de acuerdo al tiempo que le indiques para luego guardarlo en un archivo log''')
+
+    parser.add_argument('-u','--url',help='Con esta opcion proporcionas la url a realizar la peticion')
+    parser.add_argument('-d','--dir',help='Especifica la direccion a donde guardar el archivo log (Default " ./ " )', default='./')
+    parser.add_argument('-t','--time',help='Especifica el tiempo en segundos (Default: 10seg.)', default=10, type=int)
+
+    args = parser.parse_args()
     
-    n = 1
+    if  not (str(args.url).__contains__('http') or str(args.url).__contains__('https')):
+        args.url = "http://" + args.url
+        print(args.url)
+    
+    try:
+        temp = requests.get(args.url)
+    except:
+        print('Ingrese una url valida.')
+        exit()  
 
-    while n > 0:
-        n += 1
-        
-        peticion ()
-        n -= 1
+    while True:
+        peticion (args.url, args.dir, int(args.time))
 
 
-def peticion ():
-    import requests
-    import time
+def peticion (url, direc, timeSleep):
 
-    url = "https://homew0rks.herokuapp.com/"
     cabecera = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
     peticion = requests.get(url=url, headers=cabecera)
-    time.sleep(600)
     stri = "  ////////////////////////////////////////////////////////////\n"
-    stri += " /// " + url + "\tCode Status: " + str(peticion.status_code) + " //////\n"
-    stri +="////////////////////////////////////////////////////////////\n\n"
-    log(stri)
+    stri += " /// " + url + "\tCode Status: " + str(peticion.status_code) + "\n"
+    stri +="////////////////////////////////////////////////////////////\n"
+    stri += str(datetime.now()) + "\n"
+    print(stri)
+    log(stri, direc)
+    time.sleep(timeSleep)
 
 
-def log(log):
-    f = open("log.txt", mode='a')
+def log(log, dir):
+    f = open(dir + "log.txt", mode='a')
     f.write(log + "\n")
     f.close()
 
@@ -35,3 +54,5 @@ if (__name__ == "__main__"):
     except KeyboardInterrupt:
         print("Saliendo")
         exit()
+    #except TypeError:
+    #    print('Debes especificar la url')
